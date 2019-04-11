df1356b771c24e81f471a57183bf02aa9a63304d
Kate F <follett.katie@gmail.com>
2019-04-11 21:26:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,7 +21,7 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-    sql = <<- SQL
+    sql = <<-SQL
     SELECT *
     FROM students
     SQL
