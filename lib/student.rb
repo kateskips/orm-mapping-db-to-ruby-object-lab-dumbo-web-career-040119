baebbc1b0464f187d23205c0baf941d174967348
Kate F <follett.katie@gmail.com>
2019-04-11 21:39:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,6 +69,21 @@ class Student
       self.new_from_db(row)
     end
   end
+  
+  def self.first_X_students_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 10
+    LIMIT 1
+    
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  
     
     
   
