809cd232224068c755b9225496e36b11a21e2707
Kate F <follett.katie@gmail.com>
2019-04-11 21:50:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,6 +97,16 @@ class Student
   end
   
   def self.all_students_in_grade_X(num)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    
+    SQL
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
     
   
   def save
